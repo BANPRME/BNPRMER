97ec36bc347b16ef5c2d2da44ba46e529673d669
BANPRME <pdfuniversidad93@gmail.com>
2026-05-19 16:15:35 -0500

    Update bot.php

--- bot.php
+++ bot.php
@@ -2,19 +2,35 @@
 
 $token = "TU_TOKEN_NUEVO";
 
-$input = file_get_contents("php://input");
-$data = json_decode($input, true);
+$content = file_get_contents("php://input");
 
-if(isset($data["message"])){
+$data = json_decode($content, true);
+
+if (isset($data["message"])) {
 
     $chat_id = $data["message"]["chat"]["id"];
     $text = $data["message"]["text"];
 
-    $respuesta = "Hola, recibí: " . $text;
+    $mensaje = "Recibido: " . $text;
 
-    file_get_contents(
-        "https://api.telegram.org/bot$token/sendMessage?chat_id=$chat_id&text=" . urlencode($respuesta)
-    );
+    $url = "https://api.telegram.org/bot".$token."/sendMessage";
+
+    $post = [
+        'chat_id' => $chat_id,
+        'text' => $mensaje
+    ];
+
+    $options = [
+        'http' => [
+            'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+            'method'  => 'POST',
+            'content' => http_build_query($post),
+        ],
+    ];
+
+    $context = stream_context_create($options);
+
+    file_get_contents($url, false, $context);
 }
 
 echo "OK";
