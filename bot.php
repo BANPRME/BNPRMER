d3b7052e8f99480c23141301f015978d389c6864
BANPRME <pdfuniversidad93@gmail.com>
2026-05-19 17:22:18 -0500

    Update bot.php

--- bot.php
+++ bot.php
@@ -16,24 +16,7 @@ if(!$update){
     echo "OK";
     exit;
 }
-/* ========================= */
-/* MENSAJES NORMALES */
-/* ========================= */
 
-if(isset($update["message"])){
-
-  
-    $chat_id = $update["message"]["chat"]["id"] ?? '';
-    $text = $update["message"]["text"] ?? '';
-
-    file_get_contents(
-        "https://api.telegram.org/bot$token/sendMessage?" .
-        http_build_query([
-            'chat_id' => $chat_id,
-            'text' => "✅ Recibido: $text"
-        ])
-    );
-}
 /* ========================= */
 /* BOTÓN TELEGRAM */
 /* ========================= */
