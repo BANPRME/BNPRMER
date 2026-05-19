55e691fe317e2a7f0864ddc0e652c376ff974036
BANPRME <pdfuniversidad93@gmail.com>
2026-05-19 16:44:41 -0500

    Update bot.php

--- bot.php
+++ bot.php
@@ -22,6 +22,8 @@ if(!$update){
 
 if(isset($update["message"])){
 
+    file_put_contents("test.txt", "MENSAJE RECIBIDO\n", FILE_APPEND);
+
     $chat_id = $update["message"]["chat"]["id"] ?? '';
     $text = $update["message"]["text"] ?? '';
 
